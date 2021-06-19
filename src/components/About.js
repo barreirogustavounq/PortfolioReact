0943852c1270e6aaa518960d45c84c8cc6f5f6f2
Gustavo Barreiro <barreirogustavo05@gmail.com>
2021-06-19 20:31:17 -0300

    implmentado el about

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,33 @@
 import React from "react";
-
+import "../styles/About.css"
 const About = () => {
 
     return (
         <div className="mainDiv">
-            <h1>ESTOY EN EL About</h1>
+            <h1 class="tittleAbout">Sobre mí</h1>
+            <div class="aboutme">
+
+                <br />
+                <h3> Mi nombre es Gustavo Emiliano Barreiro,
+                    tengo 30 años, vivo en la localidad de Temperley partido de Lomas de Zamora.
+                    Soy estudiante de la Universidad Nacional de Quilmes,
+                    me encuentro en el ultimo año de la carrera de Tecnicatura Universitaria en Programación
+                    con aspiraciones a realizar la Licenciatura en Sistemas. Me considero una persona responsable,
+                    comprometida, alguien con quien siempre se puede dialogar, abierto a nuevas ideas. Actualmente me
+                     encuentro en la busqueda de mi primer
+                    experiencia laboral en el mundo IT, donde me gustaria poder tener la oportunidad de aplicar los conocimientos adquiridos
+                    en la Universidad como tambien de los cursos que he realizado durante todo este tiempo.</h3>
+                <br />
+                <h3>Lo mas preciado que tengo es mi hijo, Mateo, quien al momento que llego a este mundo me cambio la vida
+                    y ya no imagino esa vida sin el.
+                    Mis hobbies son el Futbol, el paddle, ver series, jugar video Juegos.
+                </h3>
+                <br />
+                <h3>
+                    Saludos.
+                </h3>
+
+            </div>
         </div>
 
     )
